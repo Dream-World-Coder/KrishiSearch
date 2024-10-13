872d31b80e771209e093a857931648a1b6241542
Subhajit Gorai <goraisubhajit345@gmail.com>
2024-10-13 15:44:09 +0530

    minor_changes

---
+++ website/js/index.js
@@ -0,0 +1,12 @@
+// js
+import "./components/menu.js";
+import "./components/news.js";
+
+// css
+import "./css/style.css";
+import "./css/fonts.css";
+import "./css/components/header.css";
+import "./css/components/main.css";
+import "./css/components/footer.css";
+import "./css/js-classes.css";
+import "./css/utils.css";
